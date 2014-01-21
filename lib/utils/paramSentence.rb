2e988279c825c65eeb1bf95d685c4528842f5a80
Daniel Bush <dlb.id.au@gmail.com>
2014-01-22 00:27:36 +1100

    Added paramSentence library into utils.

---
+++ lib/utils/paramSentence.rb
@@ -0,0 +1,65 @@
+# Randomly generate sentences.
+#
+# See spec examples in spec/.
+# 
+# Take a spec which is of form
+#   [<sentence>,<hash>]
+# where <sentence> is a string and
+# each entry in <hash>
+#   <key> => <value>
+# where <key> is a symbol, and <value>
+# is an array of <sentence>'s.
+# 
+# <sentence> is a sentence or phrase.
+# If a word starts with ':' it will be looked up
+# in <hash>, an entry from <value> will be randomly
+# selected.
+# The entry from <value> will also undergo the
+# same ':' substitution.
+# 
+
+module Beerbot
+  module Utils
+    module ParamSentence
+
+      # Generate a sentence.
+
+      def self.gen(spec)
+        sentence,params = spec
+        self.transform(sentence,params).strip
+      end
+
+      def self.expand(str,params)
+        sym = str.to_sym
+        if not params.has_key?(sym) then
+          raise "'#{str}' has no corresponding symbol in params #{params}"
+        end
+        result = params[str.to_sym].sample
+        result ? result : ""
+      end
+
+      # Split, map and rejoin a sentence (str).
+      #
+      # After splitting, apply expand on parameter words ":word".
+      #
+      # Watch out, throws error, be prepared.
+
+      def self.transform(str,params)
+        words = str.split(/\s+/)  # not great, we lose additional spaces
+        words.map {|word|
+          if word[0] == ':' then
+            if word[1] == ':' then
+              word  # let the bot code gsub this; ::from , ::to
+            else
+              #p "expanding #{word}"
+              self.transform(self.expand(word[1..-1],params),params)
+            end
+          else
+            word
+          end
+        }.select{|word| word != nil && word != ""}.join(' ')
+      end
+      
+    end
+  end
+end
---
+++ spec/utils/paramSentence_spec.rb
@@ -0,0 +1,83 @@
+require File.dirname(__FILE__)+"/../../lib/utils/paramSentence.rb"
+require 'pp'
+
+describe "param sentence generation" do
+  spec1 = [
+    ":ithink :youmust :dothis :toget :this ::from",
+    {
+      :ithink => [nil,"I think","Perhaps","Clearly"],
+      :youmust => [
+        "you must",
+        "you are going to have to",
+        "you will have to"
+      ],
+      :dothis => [
+        "spend some time thinking on this",
+        "think some more about this",
+        "think some more on this",
+        "search inside yourself",
+        "ponder this for some time"
+      ],
+      :toget => [
+        "to get",
+        "to obtain",
+        "to find"
+      ],
+      :this => [
+        "that answer",
+        "the answer",
+        "the answer to that one",
+        "the thing you want answered"
+      ],
+    }
+  ]
+
+
+  spec2 = [
+    ":expletive do you think I'm :something ?",
+    {
+      :expletive => [
+        nil,"srsly,","ffs,","seriously,",
+        "for the love of all things silicon"
+      ],
+      :something => [
+        "google",
+        "your :slave",
+        "just here to help you",
+        "just here to help little ol' you",
+        "sitting here just :towaste on :you"
+      ],
+      :slave => [
+        "slave",
+        "PA",
+        "own personal answering service",
+      ],
+      :towaste => [
+        "to :waste :precious compute cycles",
+        "to :waste my :precious cpu time",
+        "to :waste my :precious cycles"
+      ],
+      :precious => [
+        nil, "precious", "valuable"
+      ],
+      :waste => [
+        "waste", "squander", "use up", "fitter away",
+      ],
+      :you => [
+        "you",
+        "lowly carbon-based life forms :likeyou",
+        "a hapless soul :likeyou",
+      ],
+      :likeyou => ["like you","such as you"],
+    },
+  ]
+
+  it "should generate sentences with a valid spec" do
+    100.times do
+      # This is a bit crap.
+      Beerbot::Utils::ParamSentence.gen(spec1)
+    end
+  end
+
+end
+
