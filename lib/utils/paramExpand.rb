839060871e50c98298965fe92271ca16ae49513e
Daniel Bush <dlb.id.au@gmail.com>
2014-01-28 08:52:26 +1100

    Renamed paramSentence to paramExpand module.

---
+++ lib/utils/paramExpand.rb
@@ -0,0 +1,71 @@
+
+module BeerBot
+
+  module Utils
+
+    # Randomly generate sentences.
+    #
+    # See spec examples in spec/.
+    # 
+    # Take a spec which is of form
+    #   [<sentence>,<hash>]
+    # where <sentence> is a string and
+    # each entry in <hash>
+    #   <key> => <value>
+    # where <key> is a symbol, and <value>
+    # is an array of <sentence>'s.
+    # 
+    # <sentence> is a sentence or phrase.
+    # If a word starts with ':' it will be looked up
+    # in <hash>, an entry from <value> will be randomly
+    # selected.
+    # The entry from <value> will also undergo the
+    # same ':' substitution.
+
+    module ParamExpand
+
+      # Generate a sentence and expand parameters that start with
+      # a single colon ':' within that sentence.
+
+      def self.lookup(str,params,raise_error=false)
+        if not params.has_key?(str) then
+          if raise_error then
+            raise "'#{str}' has no corresponding symbol in params #{params}"
+          else
+            return ""
+          end
+        end
+        result = params[str].sample
+        result ? result : ""
+      end
+
+      # Recursively expand a sentence with parameters starting with
+      # ':' using values sampled from params.
+      # 
+      # Split, map and rejoin a sentence (str).
+      #
+      # After splitting, apply expand on parameter words ":word".
+      #
+      # Watch out, throws error, be prepared.
+
+      def self.expand(str,params,raise_error=false)
+        words = str.split(/\s+/)  # not great, we lose additional spaces
+        words.map {|word|
+          if word[0] == ':' then
+            if word[1] == ':' then
+              word  # let the bot code gsub this; ::from , ::to
+            else
+              self.expand(
+                self.lookup(word[1..-1],params,raise_error),
+                params,raise_error
+              )
+            end
+          else
+            word
+          end
+        }.select{|word| word != nil && word != ""}.join(' ').strip
+      end
+      
+    end
+  end
+end
