0368debcc8d1fbef1e0218bca96b7933dd77ed7e
Daniel Bush <dlb.id.au@gmail.com>
2014-04-26 15:33:46 +1000

    Moved modules into separate repo - see modules/README.

--- modules/Oracle/init.rb
+++
@@ -1,8 +0,0 @@
-# The files in this directory are part of BeerBot, a a ruby irc bot library.
-# Copyright (C) 2014 Daniel Bush
-# This program is distributed under the terms of the GNU
-# General Public License.  A copy of the license should be
-# enclosed with this project in the file LICENSE.  If not
-# see <http://www.gnu.org/licenses/>.
-
-require_relative 'oracle.rb'
